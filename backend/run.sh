cb8820e1f4e44e6a3f76335f03506d38fb786437
Alex Anzelm <anex.work@gmail.com>
2018-11-30 18:37:15 +0300

    ~ fix in backend

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /etc/profile.d/rvm.sh
 bundle exec rake db:setup RAILS_ENV=production
 bundle exec rake db:migrate RAILS_ENV=production
-bundle exec puma -e production
+bundle exec puma -e production -b tcp://0.0.0.0:3000
